2a1ddde2e858148dbd18d3e4856efdea1b270056
Rishabh528 <rishabh.agg96@gmail.com>
2020-01-23 11:46:14 +0530

    add to task using ENTER

--- script.js
+++ script.js
@@ -2,11 +2,19 @@ window.onload = function (){
   let item = document.getElementById('item');
   let add = document.getElementById('add');
 
-  add.onclick = function addTask (){
+  function addTask (){
     let tasklist = document.getElementById('tasklist');
     let task = document.createElement('li');
     console.log(item.value);
     task.innerText = item.value;
     tasklist.appendChild(task);
   }
+
+  add.onclick = addTask;
+
+  item.addEventListener('keyup', function(event){
+    if(event.keyCode==13){
+      addTask();
+    }
+  })
 }
